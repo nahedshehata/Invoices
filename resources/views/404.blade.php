ff5652a3ef7707f6d280a12d5e9be3fffda938e0
nahed <nahedshehata>
2023-12-28 18:43:12 +0200

    complete user

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -18,7 +18,6 @@
 			<h2>Oopps. The page you were looking for doesn't exist.</h2>
 			<h6>You may have mistyped the address or the page may have moved.</h6><a class="btn btn-outline-danger" href="{{ url('/' . $page='index') }}">Back to Home</a>
 		</div>
-		<!-- /Main-error-wrapper -->
 @endsection
 @section('js')
 @endsection
